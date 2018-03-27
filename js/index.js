3767a6a71292296b2241bc27011ad9285d1158f9
Aditya Shrivastava <adityas140797@gmail.com>
2018-03-27 17:25:44 +0530

    aa

---
+++ js/index.js
@@ -0,0 +1,72 @@
+var config = {
+    apiKey: "AIzaSyDIsNts-YS5A803UDEg-pELhwtgMmICt6w",
+    authDomain: "aditya-f14ee.firebaseapp.com",
+    databaseURL: "https://aditya-f14ee.firebaseio.com",
+    projectId: "aditya-f14ee",
+    storageBucket: "aditya-f14ee.appspot.com",
+    messagingSenderId: "719539654966"
+  };
+firebase.initializeApp(config);
+
+function abc(){
+var number = document.getElementById("number");
+var age = document.getElementById("age");
+var phone = document.getElementById("phone");
+var aadhaar = document.getElementById("aadhaar");
+
+var submitBtn = document.getElementById("submitBtn");	
+var roll = number.value;
+var age = age.value;
+var phone = phone.value;
+var aadhaar = aadhaar.value;
+var abcd = "yes";
+
+var user = firebase.auth().currentUser;
+var ab = user.displayName;
+var uid = user.uid;
+var firebaseRef = firebase.database().ref('Users').child(uid);
+
+firebase.auth().currentUser.sendEmailVerification()
+
+var data = {
+name: ab,
+roll: roll,
+age: age,
+phone: phone,
+aadhaar: aadhaar,
+//a: abcd;
+}
+
+firebaseRef.set(data);
+}
+
+function genQRcode() {
+var user = firebase.auth().currentUser;
+var uid = user.uid;
+var ab = user.displayName;
+var message;
+var firebaseRef = firebase.database().ref('Payment').child(uid);
+
+firebaseRef.on('value', function(snapshot) {
+   message = snapshot.val();
+
+    var data = encodeURIComponent("Id"+" : "+uid+"\n\n"+"Name"+" : "+message.Name+"\n\n"+"Source"+" : "+message.Source+"\n\n"+"Destination"+" : "+message.Destination+"\n\n"+"Booking Date"+" : "+message.Booking_Date+"\n\n"+"Booking Time"+" : "+message.Booking_Time),
+      size = document.getElementById("size").value,
+      chart = "http://chart.googleapis.com/chart?cht=qr&chs=" + size + "x" + size + "&choe=UTF-8&chld=L|0&chl=" + data;
+    if (data === "") {
+      alert("Please enter a valid data!");
+      textarea.focus();
+      content.style.display = "none";
+    } else {
+      content.style.display = "";
+      document.getElementById("qrcode").src = chart;
+      document.getElementById("qrcode-url").value = chart;
+    }
+	});
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.ctrlKey && e.keyCode == 13) {
+      genQRcode();
+    }
+  });
---
+++ js/index1.js
@@ -0,0 +1,70 @@
+var config = {
+    apiKey: "AIzaSyDIsNts-YS5A803UDEg-pELhwtgMmICt6w",
+    authDomain: "aditya-f14ee.firebaseapp.com",
+    databaseURL: "https://aditya-f14ee.firebaseio.com",
+    projectId: "aditya-f14ee",
+    storageBucket: "aditya-f14ee.appspot.com",
+    messagingSenderId: "719539654966"
+  };
+firebase.initializeApp(config);
+
+function aabb(){
+	
+alert(abc);
+
+var currentDate = new Date(),
+      day = currentDate.getDate(),
+      month = currentDate.getMonth() + 1,
+      year = currentDate.getFullYear();
+ var date = day + "/" + month + "/" + year;
+
+	var currentTime = new Date(),
+      hours = currentTime.getHours(),
+      minutes = currentTime.getMinutes();
+
+	if (minutes < 10) {
+	 minutes = "0" + minutes;
+  }
+	var time = hours + ":" + minutes;
+
+var b = document.getElementById("origin-input");
+var c = document.getElementById("destination-input");
+//var a = document.getElementById("dm");
+//var s1 = document.getElementById("SLan");
+//var s2 = document.getElementById("SLon");
+//var d1 = document.getElementById("DLan");
+//var d2 = document.getElementById("DLon");
+
+var submitBtn = document.getElementById("submit");	
+
+var b = b.value;
+var c = c.value;
+var a = "yes";
+//var s1 = s1.value;
+//var s2 = s2.value;
+//var d1 = d1.value;
+//var d2 = d2.value;
+
+//alert(s1);
+
+var user = firebase.auth().currentUser;
+var ab = user.displayName;
+var uid = user.uid;
+
+var firebaseRef = firebase.database().ref('Payment').child(uid);
+
+var data = {
+Name: ab,
+Source: b,
+Destination: c,
+Booking_Date: date,
+Booking_Time: time,
+Source_Latitude: a
+//Rate: a
+//Source Longitude: s2,
+//Destination Latitude: d1,
+//Destination Longitude: d2
+}
+
+firebaseRef.set(data);
+}
